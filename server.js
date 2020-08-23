f6f5b95844f0b9d2de36ae2f8fb3880396dda76b
Julie Schaub <julie.schaub@hotmail.com>
2020-08-23 11:13:32 -0500

    final files

--- server.js
+++ server.js
@@ -53,44 +53,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Notes HTML & API Routes
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
 
   app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/Develop/db/db.json"));
+    res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
 app.get("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("/Develop/db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
     res.json(savedNotes[Number(req.params.id)]);
 });
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
 
 //Save note to db.json
 app.post("/api/notes", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("/Develop/db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
     let newNote = req.body;
     newNote.id = uuid.v4(),
     savedNotes.push(newNote);
 
-    fs.writeFileSync("/Develop/db/db.json", JSON.stringify(savedNotes));
+    fs.writeFileSync("/db/db.json", JSON.stringify(savedNotes));
     console.log("Note saved to db.json. Content: ", newNote);
     res.json(savedNotes);
 })
 
 //Delete Note
 app.delete("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("/Develop/db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
     let noteID = req.params.id;
     console.log(`Deleting note with ID ${noteID}`);
     savedNotes = savedNotes.filter(currNote => {
         return currNote.id != noteID;
     })
     
-    fs.writeFileSync("/Develop/db/db.json", JSON.stringify(savedNotes));
+    fs.writeFileSync("/db/db.json", JSON.stringify(savedNotes));
     res.json(savedNotes);
 })
 
