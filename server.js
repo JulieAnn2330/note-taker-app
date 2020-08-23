e723fc71bd1ac52a5a12aeb6210c2682cba5f772
Julie Schaub <julie.schaub@hotmail.com>
2020-08-23 11:27:11 -0500

    final files

--- server.js
+++ server.js
@@ -57,11 +57,11 @@ app.get("/notes", function(req, res) {
   });
 
   app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.sendFile(path.join(__dirname, "db/db.json"));
 });
 
 app.get("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     res.json(savedNotes[Number(req.params.id)]);
 });
 
@@ -71,26 +71,26 @@ app.get("*", function(req, res) {
 
 //Save note to db.json
 app.post("/api/notes", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     let newNote = req.body;
     newNote.id = uuid.v4(),
     savedNotes.push(newNote);
 
-    fs.writeFileSync("/db/db.json", JSON.stringify(savedNotes));
+    fs.writeFileSync("db/db.json", JSON.stringify(savedNotes));
     console.log("Note saved to db.json. Content: ", newNote);
     res.json(savedNotes);
 })
 
 //Delete Note
 app.delete("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
+    let savedNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     let noteID = req.params.id;
     console.log(`Deleting note with ID ${noteID}`);
     savedNotes = savedNotes.filter(currNote => {
         return currNote.id != noteID;
     })
     
-    fs.writeFileSync("/db/db.json", JSON.stringify(savedNotes));
+    fs.writeFileSync("db/db.json", JSON.stringify(savedNotes));
     res.json(savedNotes);
 })
 
